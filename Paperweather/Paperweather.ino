c95db7886c64df708c1aaa7d4b372af76974c15a
Holden <holden@makeanything.us>
2020-12-25 02:13:44 -0500

    this is basically completed. added battery heath, ntp local time (hardcoded EST right now), and fixed the formatting around the 'extended forecast'. more robust condition parsing, and condiiton parsing for thunderstorms. the sleep functionality doesnt seem to work as advertised in this sketch or in the example sketch. still using delay for now

--- Paperweather/Paperweather.ino
+++ Paperweather/Paperweather.ino
@@ -1,5 +1,6 @@
 #include <HTTPClient.h>
 #include <WiFi.h>
+#include "time.h"
 #include "binaryttf.h"
 #include "config.h"
 #include "M5EPD.h"
@@ -7,7 +8,6 @@
 
 M5EPD_Canvas canvas1(&M5.EPD);
 HTTPClient client;
-
 JSON_Value *json_root = NULL;
 
 char *sunny = "";
@@ -17,38 +17,72 @@ char *rainy = "";
 char *tstormy = "";
 char *unknown = "U";
 
+String timeString;
+
+#define DEFAULT_TEXT_SIZE 30
+#define XRES 540
+#define YRES 960
+#define XMID XRES / 2
+#define YMID YRES / 2
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 3600;
+
+void addText(String text, int x, int y, int size)
+{
+  canvas1.setTextDatum(BL_DATUM);
+  canvas1.createRender(size, 256);
+  canvas1.setTextSize(size);
+  canvas1.setTextDatum(CC_DATUM);
+  canvas1.drawString(text, x, y);
+}
+
+void addText(String text, int x, int y)
+{
+  addText(text, x, y, DEFAULT_TEXT_SIZE);
+}
+
 void wifi_connect_and_fetch_data()
 {
   // Connect to Wifi
-  M5.EPD.Clear(true);
   int wifi_attempt_count = 0;
-  WiFi.begin(ssid, pass);
-  canvas1.setTextSize(10);
-  canvas1.drawString("Attempting wifi connection", 0, 0);
-  canvas1.pushCanvas(0, 0, UPDATE_MODE_GC16);
-  while (WiFi.status() != WL_CONNECTED) {     
-
-    if (wifi_attempt_count % 10 == 0)
+  while (WiFi.status() != WL_CONNECTED) {
+    if (wifi_attempt_count % 5 == 0)
     {
       //Flash the screen to know we are still alive and trying to connect to wifi
       //Only do this once every 5 seconds
-      M5.EPD.Clear(true);
-      canvas1.setTextSize(10);
-      canvas1.drawString("Attempting wifi connection", 0, 0);
-      canvas1.pushCanvas(0, 0, UPDATE_MODE_GC16);
-
+      
       //Restart the wifi connect attempt..
       //FIXME: This still isn't great. We should probably check for an error status on the wifi connect attempt (assuming such a result is supplied by the Wifi API)
-      WiFi.begin(ssid, pass);
-      
-      delay(500);
-      
-      //FIXME: Put in an upper bound on attempts before giving up and just displaying the previously captured (or default) info
-      //        -- This is critical for the "low power" approach          
-    }
+      Serial.println("Re-trying wifi connection: " + String(wifi_attempt_count));
+      WiFi.begin(ssid, pass);    
 
+      if (wifi_attempt_count >= 30)
+      {
+        //FIXME: Display previous (or default) data??
+        M5.EPD.Clear(true);
+        delay(100);
+        addText(String("Failed to connect to wifi"), XMID, YMID - 30);
+        canvas1.pushCanvas(0, 0, UPDATE_MODE_GC16);
+
+        delay(1000 * refreshintervalseconds);
+        wifi_attempt_count = -1;
+        M5.EPD.Clear(true);
+        
+      }
+      else
+      {
+        addText(String("Attempting wifi connection"), XMID, YMID - 30);
+      }
+      canvas1.drawString("SSID: " + String(ssid), XMID, YMID);
+      canvas1.drawString("Pass: " + String(pass), XMID, YMID+DEFAULT_TEXT_SIZE);
+      canvas1.pushCanvas(0, 0, UPDATE_MODE_GC16);
+    }
+    
+    delay(1000);
     wifi_attempt_count++;
-  }
+  }  
 
   // Fetch weather JSON
   client.begin(weatherurl);
@@ -68,30 +102,67 @@ void wifi_connect_and_fetch_data()
   json_root = json_parse_string(noaabuffer.c_str());
 }
 
+void getNtpTime()
+{
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  struct tm timeinfo;
+
+  if(!getLocalTime(&timeinfo)){
+    timeString = "UnknownTime";    
+    return;
+  }
+  timeinfo.tm_hour -= 5;
+  char buf[40];  
+  strftime(buf, 40, "%m/%d/%y %H:%M", &timeinfo);
+  timeString = String(buf);
+
+  // FIXME: Maybe add some smart logic to look at current real time and last 
+  //        report time and try to synchronize our next sleep so we always have
+  //        up to date data as it is published
+
+}
+
 void wifi_disconnect()
 {
   WiFi.disconnect();
+}
+
+void draw_lastUpdated()
+{
+  const char *datenow = json_object_dotget_string(json_value_get_object(json_root), "creationDateLocal"); 
+  addText(String("Last Updated: " + String(datenow)), XMID, 910, 20);
+}
+
+void draw_batteryleft()
+{
+  char buf[20];
+  uint32_t vol = M5.getBatteryVoltage();
+  float battery = (float)(vol - 3300) / (float)(4350 - 3300);
+  if(battery <= 0.01)
+  {
+      battery = 0.01;
+  }
+  if(battery > 1)
+  {
+      battery = 1;
+  }
+  uint8_t px = battery * 25;
+  sprintf(buf, "Battery: %d%%", (int)(battery * 100));
+  addText(String(buf) , XMID, 40, 20);
 }
 
 void draw_topbar()
 {
   const char *datenow = json_object_dotget_string(json_value_get_object(json_root), "currentobservation.Date");
   const char *area = json_object_dotget_string(json_value_get_object(json_root), "location.areaDescription");
-
-  // Wifi network indicator
-  canvas1.createRender(20, 256);
-  canvas1.setTextSize(20);
-  canvas1.setTextDatum(CL_DATUM);
-  canvas1.drawString(datenow, 20, 20);
+  
+  addText(timeString, 20, 20, 20);
 
   // Area
-  canvas1.createRender(20, 256);
-  canvas1.setTextSize(20);
-  canvas1.setTextDatum(CR_DATUM);
-  canvas1.drawString(area, 520, 20);
+  addText(area, 520, 20, 20);  
 }
 
-void draw_weathernow()
+void draw_weathericon()
 {
   const char *tempnow = json_object_dotget_string(json_value_get_object(json_root), "currentobservation.Temp");
   const char *descnow  = json_object_dotget_string(json_value_get_object(json_root), "currentobservation.Weather");
@@ -103,39 +174,44 @@ void draw_weathernow()
   canvas1.setTextDatum(CC_DATUM);
 
   char *symbol;
-  if      (!strcmp(descnow, "Chance Flurries")) symbol = snowy;
-  else if (!strcmp(descnow, "Flurries")) symbol = snowy;
+  if (strstr(descnow,"Flur") != NULL) symbol = snowy;
+  else if (strstr(descnow,"Snow") != NULL) symbol = snowy;
+  else if (strstr(descnow,"Winter") != NULL) symbol = snowy;  
+  else if (strstr(descnow,"Blizzard") != NULL) symbol = snowy;
   //
-  else if (!strcmp(descnow, "Fair"))         symbol = sunny;
-  else if (!strcmp(descnow, "Clear"))        symbol = sunny;
-  else if (!strcmp(descnow, "Mostly Clear")) symbol = sunny;
-  else if (!strcmp(descnow, "Mostly Sunny")) symbol = sunny;
-  else if (!strcmp(descnow, "Sunny"))        symbol = sunny;
+  else if (strstr(descnow,"Fair") != NULL) symbol = sunny;
+  else if (strstr(descnow,"Clear") != NULL) symbol = sunny;
+  else if (strstr(descnow,"Sun") != NULL) symbol = sunny;
   //
-  else if (!strcmp(descnow, "Partly Cloudy")) symbol = cloudy;
-  else if (!strcmp(descnow, "Cloudy"))        symbol = cloudy;
-  else if (!strcmp(descnow, "Overcast"))      symbol = cloudy;
-  else if (!strcmp(descnow, "Mostly Cloudy")) symbol = cloudy;
+  else if (strstr(descnow,"Cloud") != NULL) symbol = cloudy;
+  else if (strstr(descnow,"Overcast") != NULL) symbol = cloudy;
   //
-  else if (!strcmp(descnow, "Chance Rain")) symbol = rainy;
-  else if (!strcmp(descnow, "Chance Showers")) symbol = rainy;
-  else if (!strcmp(descnow, "Rain Likely")) symbol = rainy;
-  else if (!strcmp(descnow, "Mostly Cloudy then Slight Chance Rain")) symbol = rainy;
-  else if (!strcmp(descnow, "Slight Chance Rain")) symbol = rainy;
+  else if (strstr(descnow,"Showers") != NULL) symbol = rainy;
+  else if (strstr(descnow,"Rain") != NULL) symbol = rainy;
+  // 
+  else if (strstr(descnow,"Thun") != NULL) symbol = tstormy; 
   //
   else symbol = unknown;
-  canvas1.drawString(symbol, 270, 240);
+  if (symbol == unknown)
+  {
+    canvas1.setTextSize(20);
+    canvas1.drawString(String(descnow), XMID, 210);
+  }
+  else
+  {
+    canvas1.drawString(symbol, XMID, 210);
+  }
 
   // Temp degrees
   canvas1.setTextSize(100);
   canvas1.setTextDatum(CC_DATUM);
-  canvas1.drawString(tempnow, 270, 350);
+  canvas1.drawString(String(tempnow)+" °F", XMID, 320);
 
   // Desc of now
   canvas1.createRender(40, 256);
   canvas1.setTextSize(40);
   canvas1.setTextDatum(CC_DATUM);
-  canvas1.drawString(descnow, 270, 440);
+  canvas1.drawString(descnow, XMID, 410);
 }
 
 void draw_forecast() {
@@ -145,18 +221,20 @@ void draw_forecast() {
   const JSON_Array *desc_long= json_object_dotget_array(json_value_get_object(json_root), "data.text");
 
   char buf[300];
+  char buf2[50];
   int i;
   for (i = 0; i < json_array_get_count(pname) && i < 5; i++) {
     const char *periodtime = json_array_get_string(pname, i);
-    const char *periodtemp = json_array_get_string(temps, i);
     const char *perioddesc = json_array_get_string(desc_short, i);
-    sprintf(buf, "%s: %s°F / %s\n", periodtime, periodtemp, perioddesc);
-    canvas1.createRender(28, 256);
-    canvas1.setTextSize(28);
-    canvas1.setTextDatum(CC_DATUM);
-    canvas1.drawString(buf, 270, 500 + (i * 38));
+    const char *highTemp = json_array_get_string(temps, (2*i)+1);
+    const char *lowTemp = json_array_get_string(temps, (2*i));
+    sprintf(buf, "%s: %s / %s °F", periodtime, highTemp, lowTemp);
+    sprintf(buf2, "%s", perioddesc);
+    addText(buf, XMID, 460 + (i*87), 30);    
+    addText(buf2, XMID, 460 + (i*87)+30, 25);
   }
 }
+
 
 void draw_sensors()
 {
@@ -168,34 +246,27 @@ void draw_sensors()
   dtostrf(tem, 2, 2 , temStr);
   dtostrf(hum, 2, 2 , humStr);
 
-  canvas1.createRender(20, 256);
-  canvas1.setTextSize(20);
-
-  canvas1.setTextDatum(CL_DATUM);
-  canvas1.drawString("Inside: " + String(temStr) + "°F" , 20, 940);
-
-  canvas1.setTextDatum(CR_DATUM);
-  canvas1.drawString("Humidity: " +  String(humStr)+ "%" , 520, 940);
+  addText(String("Inside: " + String(temStr) + "°F"), 20, 940, 20);
+  addText(String("Humidity: " +  String(humStr)+ "%" ), 520, 940, 20);
 }
-
-
-
-
-
-
 
 void draw()
 {
+  canvas1.fillCanvas(0);
   wifi_connect_and_fetch_data();
-  //Clear the screen after "connect and fetch".
-  //The connect and fetch function handles clearing the screen on its own during wifi status"ing"
   M5.EPD.Clear(true);
+  canvas1.fillCanvas(0);
   
+  getNtpTime();
   draw_topbar();
-  draw_weathernow();
+  draw_batteryleft();
+  draw_weathericon();
   draw_forecast();
+  draw_lastUpdated();
   draw_sensors();
+
   canvas1.pushCanvas(0, 0, UPDATE_MODE_GC16);
+
   wifi_disconnect();
   json_value_free(json_root);
 }
@@ -205,7 +276,6 @@ void setup()
   M5.begin();
   M5.TP.SetRotation(90);
   M5.EPD.SetRotation(90);
-  M5.EPD.Clear(true);
   M5.SHT30.Begin();
   M5.RTC.begin();
   canvas1.loadFont(binaryttf, sizeof(binaryttf)); // Load font files from binary data
