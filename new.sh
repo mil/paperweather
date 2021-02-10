676a79081374e8ded779c9d13a01c1173afc89b9
Miles Alan <m@milesalan.com>
2021-02-09 19:36:55 -0600

    Delete unused script

--- new.sh
+++
@@ -1,5 +0,0 @@
-arduino-cli --additional-urls "$ADDURLS" --additional-urls "$M5JSON" core install "$ESPCORE"
-arduino-cli --additional-urls "$ADDURLS" lib update-index
-arduino-cli --additional-urls "$ADDURLS" --additional-urls "$ESPJSON" lib install M5Stack
-arduino-cli --additional-urls "$ADDURLS" core update-index
-arduino-cli --additional-urls "$ADDURLS" core install m5stack:esp32
